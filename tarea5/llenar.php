b5a105483d4fa92e0bf3128cf29ec49823767a4c
Dani <dani.dass.do@gmail.com>
2020-03-25 15:59:29 -0400

    tarea5

---
+++ tarea5/llenar.php
@@ -0,0 +1,23 @@
+
+<form action="restas.php" method="POST">
+<?php 
+    $n = $_POST['n'];
+    for($i=1; $i<=$n; $i++){
+        echo'
+                <input type="text" name="i'.$i.'" style="width: 50px; height: 50px;">
+            ';
+    }
+    echo'</br>';
+    for($j=1; $j<=$n; $j++){
+        echo'
+            <input type="text" name="j'.$j.'" style="width: 50px; height: 50px;">
+        ';
+    }
+    echo '<br><input type="submit">';
+
+    setcookie('cookie',$n);
+?>
+
+
+</form>
+
