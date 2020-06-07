dc5ce687f14c1706e7822ef9e6af1914517fd35a
Dani <dani.dass.do@gmail.com>
2020-06-07 16:40:47 -0400

    tarea17 bootstrap AJAX

---
+++ tarea18/autenticar.php
@@ -0,0 +1,7 @@
+<?php
+$correo =$_POST['correo'];
+$password =$_POST['password'];
+
+    echo '<p>Correo: </p> <p class="text-muted">'.$correo.'</p>';
+    echo  '<p>Contraseña: </p> <p class="text-muted">'. $password.'</p>';
+?>
