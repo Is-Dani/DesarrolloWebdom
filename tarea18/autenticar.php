79ea1da6237b57b81b2107625a0f6e8a40eb5896
Dani <dani.dass.do@gmail.com>
2020-06-07 16:50:09 -0400

    Revert "tarea17 bootstrap AJAX"
    This reverts commit dc5ce687f14c1706e7822ef9e6af1914517fd35a.

--- tarea18/autenticar.php
+++
@@ -1,7 +0,0 @@
-<?php
-$correo =$_POST['correo'];
-$password =$_POST['password'];
-
-    echo '<p>Correo: </p> <p class="text-muted">'.$correo.'</p>';
-    echo  '<p>Contraseña: </p> <p class="text-muted">'. $password.'</p>';
-?>
